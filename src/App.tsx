510a8c99e111701826df27394d84b07ad2f578f2
OmAmberkar <omamberkar08@gmail.com>
2025-06-01 22:33:17 +0530

    initializing

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+  <div className="flex justify-center bg-gray-500 items-center h-screen text-3xl underline">
+  Hello world!
+</div>
+
+  )
+}
+
+export default App
